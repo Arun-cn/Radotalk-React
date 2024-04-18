e985c813fea56b79eea9d2754f32041fabe5ce90
Arun-cn <arunchandran4321@gmail.com>
2024-04-18 16:08:13 +0530

    axios upadate

--- src/axios/axiosInstance.js
+++ src/axios/axiosInstance.js
@@ -8,19 +8,4 @@ const axiosInstance = axios.create({
                     // Add any additional headers you need for authentication or other purposes
           }
         });
-
-        // Add a request interceptor to include the JWT token in the headers if available
-        axiosInstance.interceptors.request.use(
-              config => {
-                    const token = localStorage.getItem('jwtToken'); // Assuming you store the JWT token in local storage
-                        if (token) {
-                          ui
-                                  config.headers.Authorization = `Bearer ${token}`;
-                        }
-                            return config;
-                    },
-                      error => {
-                            return Promise.reject(error);
-                      });
-
-                      export default axiosInstance;
+ export default axiosInstance;
