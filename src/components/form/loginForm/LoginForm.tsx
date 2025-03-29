92b3d403dc99c7f133562663470b9adbea7d76d5
Arun-cn <arunchandran9876@gmail.com>
2025-03-29 12:42:41 +0530

    feat: add UI components for login form and card layout, integrate react-hook-form for form handling

---
+++ src/components/form/loginForm/LoginForm.tsx
@@ -0,0 +1,77 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+// Define Login Zod schema
+const loginSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(6, "Password must be at least 6 characters long"),
+});
+
+export function LoginForm() {
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof loginSchema>) {
+    console.log(data);
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-5 max-w-3xl mx-auto pt-4 pb-2"
+      >
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>email</FormLabel>
+              <FormControl>
+                <Input placeholder="email" type="" {...field} />
+              </FormControl>
+              <FormDescription>This is your email.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="Password" {...field} />
+              </FormControl>
+              <FormDescription>Enter your password.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
+
+export default LoginForm;
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,28 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import LoginForm from "@/components/form/loginForm/LoginForm";
+
 const LoginPage = () => {
   return (
-    <div className="bg-amber-800 w-full h-full">
-      <h1 className="flex justify-center items-center">LoginPage</h1>
+    <div className="bg-secondary w-full  h-screen flex justify-center items-top sm:items-center">
+      <Card className="w-96 max-h-120 gap-0 mt-7 sm:mt-0">
+        <CardHeader className="mb-0.5">
+          <CardTitle>Login</CardTitle>
+          <CardDescription>to get started</CardDescription>
+        </CardHeader>
+        <CardContent className="mt-0">
+          <LoginForm />
+        </CardContent>
+        <CardFooter>
+          <p className="text-[12px] pl-1">New User?</p>
+        </CardFooter>
+      </Card>
     </div>
   );
 };
